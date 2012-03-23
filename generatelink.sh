74d33d68849b4d20df616e5bf125304e112c6e1f
Sheile <sheile1024@gmail.com>
2012-03-23 09:43:56 +0900

    既存の設定ファイルをbackup以下に退避するように変更

--- generatelink.sh
+++ generatelink.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
 cd $(dirname $0)
+if [ ! -d backup ]
+then
+    mkdir backup
+fi
+
 for dotfile in .?*
 do
     if [ $dotfile != '..' ] && [ $dotfile != '.git' ]
     then
+        if [ -f "$HOME/$dotfile" ] && [ ! -L "$HOME/$dotfile" ]
+        then
+            mv "$HOME/$dotfile" "$PWD/backup"
+        fi
         ln -Fs "$PWD/$dotfile" $HOME
     fi
 done
