1023dc6e5fe53ad7571277a8c2851c1db43eb872
Sheile <sheile1024@gmail.com>
2012-03-30 15:23:55 +0900

    記述方法をリファクタリング／内容の変更は無し

--- applydotfiles.sh
+++ applydotfiles.sh
@@ -1,17 +1,11 @@
 #!/bin/sh
 
 cd $(dirname $0)
-if [ ! -d backup ]
-then
-    mkdir backup
-fi
+[ ! -d backup ] && mkdir backup
 
-for dotfile in .?*
-do
-    if [ $dotfile != '..' ] && [ $dotfile != '.git' ]
-    then
-        if [ -f "$HOME/$dotfile" ] && [ ! -L "$HOME/$dotfile" ]
-        then
+for dotfile in .?*; do
+    if [ $dotfile != '..' ] && [ $dotfile != '.git' ]; then
+        if [ -f "$HOME/$dotfile" ] && [ ! -L "$HOME/$dotfile" ]; then
             mv "$HOME/$dotfile" "$PWD/backup"
         fi
         ln -Fs "$PWD/$dotfile" $HOME
