1af24e23ff6db963184698db7f240cfa5212c422
Sheile <sheile1024@gmail.com>
2024-03-24 23:37:37 +0900

    Add lazy.nvim to manage packages

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,24 @@
 require('helpers')
 
 vim.scriptencoding = 'utf-8'
+vim.g.mapleader = ' '
+
+-- plugins(lazy.nvim)
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require('lazy').setup('plugins')
 
 -- general configuration
-vim.g.mapleader = ' '
 vim.opt.number = true
 vim.opt.showmatch = true
 vim.opt.matchtime = 3
