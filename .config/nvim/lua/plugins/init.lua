e8165f52d3880c20f778d38567177da7273db545
Sheile <sheile1024@gmail.com>
2024-04-17 23:43:45 +0900

    Add nvim-treesitter explicitly with configuration

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -7,6 +7,24 @@ return {
   },
   'github/copilot.vim',
   {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    config = function()
+      local configs = require('nvim-treesitter.configs')
+      configs.setup({
+        ensure_installed = {
+          'c', 'lua', 'vim', 'vimdoc', 'query', 'bash', 'javascript', 'html', 'css', 'csv',
+          'git_config', 'git_rebase', 'gitcommit', 'gitignore', 'json', 'markdown_inline',
+          'perl', 'php', 'python', 'requirements', 'ruby', 'rust', 'scss', 'sql',
+          'ssh_config', 'tmux', 'toml', 'typescript', 'xml', 'yaml'
+        },
+        sync_install = false,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end
+  },
+  {
     'EdenEast/nightfox.nvim',
     lazy = false,
     priority = 1000,
