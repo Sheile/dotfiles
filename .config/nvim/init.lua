3f068c05d91db0f81602438eb04619fe89d1b115
Sheile <sheile1024@gmail.com>
2024-10-21 22:17:27 +0900

    Load plugins after general configuration due to use it while initialize plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,21 +1,6 @@
 vim.scriptencoding = 'utf-8'
 vim.g.mapleader = ' '
 
--- plugins(lazy.nvim)
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-require('lazy').setup('plugins')
-
 -- general configuration
 vim.opt.number = true
 vim.opt.cursorline = true
@@ -53,3 +38,18 @@ vim.api.nvim_create_autocmd( 'InsertLeave', {
   group = 'default',
   callback = function() vim.opt.paste = false end
 })
+
+-- plugins(lazy.nvim)
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require('lazy').setup('plugins')
