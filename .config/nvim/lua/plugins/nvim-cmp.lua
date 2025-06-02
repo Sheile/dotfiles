db09e5588d4ee08dbb59d995bf0ea81d4a983b0b
Sheile <sheile1024@gmail.com>
2025-06-02 14:42:55 +0900

    Add cmp-nvim-lsp to complete by LSP

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -3,6 +3,7 @@ return {
     'hrsh7th/nvim-cmp',
     event = 'InsertEnter',
     dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-path',
     },
@@ -11,11 +12,25 @@ return {
 
       cmp.setup({
         sources = {
+          { name = 'nvim_lsp' },
           { name = 'buffer' },
           { name = 'path', { trailing_slash = true } },
         },
         mapping = cmp.mapping.preset.insert({
-          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-d>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping(function(fallback)
+            -- Complete by nvim_lsp only if complete after dot.
+            local col = vim.fn.col('.') - 1
+            local prev_char = string.sub(vim.fn.getline('.'), col, col)
+            local option = {}
+            if prev_char == '.' then
+              option = { config = { sources = { { name = 'nvim_lsp' } } } }
+            end
+            if not cmp.complete(option) then
+              fallback()
+            end
+          end),
           ['<Tab>'] = cmp.mapping.confirm { select = true },
           ['<CR>'] = cmp.mapping.confirm { select = true },
           ['<C-CR>'] = cmp.mapping.confirm { select = true },
