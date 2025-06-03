02b09d92055e8e4b3b8c1a5db4e6a98734ab0e4e
Sheile <sheile1024@gmail.com>
2025-06-03 10:21:53 +0900

    Use latest version of telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
   {
-    'nvim-telescope/telescope.nvim', branch = '0.1.x',
+    'nvim-telescope/telescope.nvim',
     dependencies = { 'nvim-lua/plenary.nvim', 'nvim-treesitter/nvim-treesitter' },
     keys = {
       { '<Leader>j', '<Cmd>Telescope buffers<CR>', mode = 'n' },
