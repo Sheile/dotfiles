59f75afa0219e72171036ca117ef629f673e23e9
Sheile <sheile1024@gmail.com>
2025-07-15 18:18:57 +0900

    feat(telescope/sorter.lua): add support for escaped spaces in search string to search term that contains space
    refactor(telescope/sorter.lua): implement custom split function to handle unescaped spaces for better term extraction

--- .config/nvim/lua/plugins/telescope/sorter.lua
+++ .config/nvim/lua/plugins/telescope/sorter.lua
@@ -1,15 +1,53 @@
 -- Patched the get_substr_matcher sorter defined in Telescope
 -- - Prefer an exact match
 -- - Prefer a prefix match on the first word (split by spaces)
+-- - Support escaped spaces (\ ) for searching strings containing spaces
 return function()
   local util = require "telescope.utils"
   local Sorter = require('telescope.sorters').Sorter
+
+  -- Split by spaces that are not preceded by backslash
+  local function split_by_unescaped_spaces(str)
+    local terms = {}
+    local current_word = ""
+    local i = 1
+
+    while i <= #str do
+      local char = str:sub(i, i)
+      local next_char = str:sub(i + 1, i + 1)
+
+      if char == "\\" and next_char == " " then
+        -- Escaped space: add space to current word
+        current_word = current_word .. next_char
+        i = i + 2
+      elseif char == " " then
+        -- Unescaped space: end current word and start new one
+        if #current_word > 0 then
+          table.insert(terms, current_word)
+          current_word = ""
+        end
+        i = i + 1
+      else
+        -- Regular character: add to current word
+        current_word = current_word .. char
+        i = i + 1
+      end
+    end
+
+    -- Add the last word if it exists
+    if #current_word > 0 then
+      table.insert(terms, current_word)
+    end
+
+    return terms
+  end
+
   local substr_highlighter = function(make_display)
     return function(_, prompt, display)
       local highlights = {}
       display = make_display(prompt, display)
 
-      local search_terms = util.max_split(prompt, "%s")
+      local search_terms = split_by_unescaped_spaces(prompt)
       local hl_start, hl_end
 
       for _, word in pairs(search_terms) do
@@ -44,7 +82,8 @@ return function()
         return 1
       end
 
-      local search_terms = util.max_split(prompt, "%s")
+      local search_terms = split_by_unescaped_spaces(prompt)
+
       for _, word in pairs(search_terms) do
         if not display:find(word, 1, true) then
           return -1
