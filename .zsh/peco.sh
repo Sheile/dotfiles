ee953fcd69ded258a1b8a736880b2c0d7d383c70
Sheile <sheile1024@gmail.com>
2025-07-30 17:34:50 +0900

    remove(peco): remove selecting tmux window with peco by ^t, space
    Because this key binding does not work in not shell(vi, python, etc.)

--- .zsh/peco.sh
+++ .zsh/peco.sh
@@ -7,13 +7,3 @@ function peco-tmux-window() {
 }
 zle -N peco-tmux-window
 bindkey '^tw' peco-tmux-window
-
-# pecoを使ったtmux window選択（全セッション）
-function peco-tmux-window-all() {
-  local selected_window=$(tmux list-windows -a -F "#{session_name}:#{window_index}:#{window_name}" | peco --query "$LBUFFER" | cut -d: -f1,2)
-  if [ -n "$selected_window" ]; then
-    tmux switch-client -t "$selected_window"
-  fi
-}
-zle -N peco-tmux-window-all
-bindkey '^t ' peco-tmux-window-all
